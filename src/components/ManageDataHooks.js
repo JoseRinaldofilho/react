7f1f6823320cc6547f627a03adb9f390eaf8eeb3
Jose Rinaldo de Vasconcelos Filho <jose.rinaldo@presidencia.gov.br>
2022-04-13 14:18:12 -0300

    criado a lista redenrizada

--- src/components/ManageDataHooks.js
+++ src/components/ManageDataHooks.js
@@ -7,6 +7,7 @@ const ManageDataHooks = () => {
 
    const [number, setNumber] = useState(10); // tem que importa
    console.log(number);
+   const [name1, setName1] = useState(8);
 
     return (
         <div>
@@ -18,6 +19,10 @@ const ManageDataHooks = () => {
             <div>
                 <p>Valor com hooks: {number} </p>
                 <button onClick={() => setNumber(25)}>muda estado</button>
+            </div>
+            <div>
+                <p>Valor com hooks: {name1} </p>
+                <button onClick={() => setName1(20)}>muda estado</button>
             </div>
 
         </div>
